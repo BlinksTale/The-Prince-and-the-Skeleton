3d03e71317ffc0f7b7f0b6cefa74647c2eae2a0b
Brian Handy <BlinksTale@gmail.com>
2015-11-12 17:58:45 -0800

    Basic movement (kind of) added.

---
+++ The Prince and the Skeleton/Assets/Controls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rigid.AddForce(new Vector2(-10f, 0f));
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rigid.AddForce(new Vector2(10f, 0f));
+		}
+	}
+}
