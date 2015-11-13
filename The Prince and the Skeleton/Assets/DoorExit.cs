bb4f48f2dc3748da9d33a12d2d067fa363d82cac
Brian Handy <BlinksTale@gmail.com>
2015-11-13 00:07:36 -0800

    Added some color materials, implemented preliminary door exit and game controller code (to manage gameplay).

---
+++ The Prince and the Skeleton/Assets/DoorExit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter2D (Collider2D c) {
+		// On triggered, restart level
+		Debug.Log ("Door triggered with " + c.gameObject.name);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
