36d128e927b1d7b488e2060c7c0cf400d92d2570
Brian Handy <BlinksTale@gmail.com>
2015-11-13 15:43:26 -0800

    Early attempt at climable rock walls, tweak to player collider to not snag on anything.

---
+++ The Prince and the Skeleton/Assets/Climable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Climable : MonoBehaviour {
+
+	GameObject subject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update() {
+//		if (subject != null) {
+//			subject.GetComponent<Rigidbody2D>().velocity *= 3f/4f;
+//		}
+	}
+
+	void OnTriggerEnter2D (Collider2D c) {
+		// On triggered, restart level
+//		if (c.gameObject.tag == "Player") {
+//			c.GetComponent<Rigidbody2D>().gravityScale = 0;
+//			subject = c.gameObject;
+//		}
+	}
+
+	void OnTriggerExit2D (Collider2D c) {
+		// On triggered, restart level
+//		if (c.gameObject.tag == "Player") {
+//			c.GetComponent<Rigidbody2D>().gravityScale = 1;
+//			subject = null;
+//		}
+	}
+}
--- The Prince and the Skeleton/Assets/Scripts/Controls.cs
+++ The Prince and the Skeleton/Assets/Scripts/Controls.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Controls : MonoBehaviour {
 
 	Rigidbody2D rigid;
+	int rockWalls = 0;
+	bool climbingWalls = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,44 @@ public class Controls : MonoBehaviour {
 		}
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			rigid.AddForce(new Vector2(10f, 0f));
+		}
+
+		if (rockWalls > 0) {
+			if (climbingWalls) {
+				rigid.velocity *= 3f/4f;
+
+				if (Input.GetKey(KeyCode.UpArrow)) {
+					rigid.AddForce(new Vector2(0f, 10f));
+				}
+				if (Input.GetKey(KeyCode.DownArrow)) {
+					rigid.AddForce(new Vector2(0f, -10f));
+				}
+			} else {
+				if (Input.GetKey(KeyCode.UpArrow)) {
+					climbingWalls = true;
+					rigid.gravityScale = 0;
+				}
+			}
+		} else {
+			if (climbingWalls) {
+				climbingWalls = false;
+				rigid.gravityScale = 1;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		Debug.Log ("Collided with " + c.gameObject.name);
+		if (c.gameObject.tag == "RockWall") {
+			rockWalls++;
+			Debug.Log ("Entered RockWall");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c) {
+		if (c.gameObject.tag == "RockWall") {
+			rockWalls--;
+			Debug.Log ("Exited RockWall");
 		}
 	}
 }
