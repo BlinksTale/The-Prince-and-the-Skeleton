4e2da227e1fbcacbe3696388c6e93c6f40bdffee
Brian Handy <BlinksTale@gmail.com>
2015-11-16 01:25:17 -0800

    Moved m4as, created wavs, tweaked gameplay, and created cutscenes (implementing Prologue)

---
+++ The Prince and the Skeleton/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SoundImages
+{
+	public AudioClip sound;
+	public GameObject image;
+}
+
+public class Cutscene : MonoBehaviour {
+
+	public SoundImages[] list;
+	int position = 0;
+	AudioClip currentAudio = null;
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = this.GetComponent<AudioSource>();
+		source.clip = list[0].sound;
+		source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!source.isPlaying) {
+			position++;
+			if (position < list.Length) {
+				// Update image and start new sound
+				list[position-1].image.SetActive(false);
+				list[position].image.SetActive(true);
+				source.clip = list[position].sound;
+				source.Play();
+			} else {
+				Application.LoadLevel(Application.loadedLevel + 1);
+			}
+		}
+	}
+}
