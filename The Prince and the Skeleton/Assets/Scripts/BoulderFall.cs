1cb384c85c7798d0aafeafb6648f32882fd3db8a
jsohn96 <jsohn96@hotmail.com>
2015-11-16 16:42:06 -0800

    Boulder Fall (Trigger) Prefab Added, visible in CH1 GamePlay

---
+++ The Prince and the Skeleton/Assets/Scripts/BoulderFall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderFall : MonoBehaviour {
+	bool _isTriggered = false; //Check for Trigger Once
+
+	void OnTriggerEnter2D(Collider2D col) {
+		//Check for Player Entering Area of Rock Fall
+		if (_isTriggered == false && col.gameObject.tag == "Player") {
+			Transform boulder = this.gameObject.transform.GetChild (0);
+			boulder.parent = null;
+			boulder.GetComponent<Rigidbody2D>().isKinematic = false;
+			_isTriggered = true;
+		}
+	}
+}
