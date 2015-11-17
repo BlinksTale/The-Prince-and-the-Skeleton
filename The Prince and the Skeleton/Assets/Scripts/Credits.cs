f8adc8ca0cff9595a0ed781f38fa18d00c68226b
Brian Handy <BlinksTale@gmail.com>
2015-11-17 13:38:43 -0800

    Credits added

---
+++ The Prince and the Skeleton/Assets/Scripts/Credits.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public string[] credits;
+	TextMesh text;
+	int index = 0;
+	public float delay = 4f;
+	public bool loop = false;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<TextMesh>();
+		TryNextCredits();
+	}
+	
+	// Update is called once per frame
+	void TryNextCredits () {
+		if (index >= credits.Length) {
+			if (!loop) {
+				HideText();
+			} else {
+				index=1;
+				NextCredits();
+			}
+		} else {
+			NextCredits();
+		}
+	}
+
+	void NextCredits() {
+		text.text = credits[index];
+		index++;
+		Invoke("TryNextCredits", delay);
+		Invoke ("HideText", delay*.75f); // so credits disappear on third beat to reappear on fourth
+	}
+
+	void HideText () {
+		text.text = "";
+	}
+}
