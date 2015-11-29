99c36a73600f6ee1d96d9c00a2508ef70bc4fbee
jsohn96 <jsohn96@hotmail.com>
2015-11-29 12:51:05 -0800

    Texture/Scene/Audio Test Files for Level Building Purposes
    In order to have the same baseline with Xiaoliang.

---
+++ The Prince and the Skeleton/Assets/Scripts/AudioTriggers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTriggers : MonoBehaviour {
+
+    bool call = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+        if(!call){
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            call = true;
+        }
+    }
+}
