ff3ce965f37d941332db24edfc7e50351a0beaae
sxl <shenxia3@mail.utoronto.ca>
2015-12-01 23:23:42 -0800

    game play 2 & 3 background

---
+++ The Prince and the Skeleton/Assets/Scripts/Guardian.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guardian : MonoBehaviour {
+
+	bool direction = false;
+	
+	void Update () {
+		Vector3 pos =  GetComponent<Transform>().localPosition;
+		
+		if(direction){
+			pos.y += 0.01f;
+			GetComponent<Transform>().localPosition = pos;
+			if(pos.y > 1.5f){
+				direction = false;
+			}
+		}
+		else {
+			pos.y -= 0.01f;
+			GetComponent<Transform>().localPosition = pos;
+			if(pos.y < -0.1f){
+				direction = true;
+			}
+		}
+	}
+}
