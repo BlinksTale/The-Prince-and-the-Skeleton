a5728189482421c8ec3176f56622525a8d7f0a4b
Brian Handy <BlinksTale@gmail.com>
2015-12-01 20:35:41 -0800

    Prologue new map, first draft

---
+++ The Prince and the Skeleton/Assets/Scripts/PrologueGameplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrologueGameplay : MonoBehaviour {
+
+	public GameObject[] tasks; // touch these in order to progress
+	int index = 0;
+	TextMesh text;
+
+	// Use this for initialization
+	void Start () {
+		foreach(GameObject g in tasks) {
+			g.GetComponent<PrologueTask>().gameplay = this;
+			g.SetActive(false);
+		}
+		tasks[0].SetActive(true);
+		text = this.GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Progress() {
+		index++;
+		if (index < tasks.Length) {
+			tasks[index].SetActive(true);
+			text.text = "Task " + (index + 1);
+		} else {
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
---
+++ The Prince and the Skeleton/Assets/Scripts/PrologueTask.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrologueTask : MonoBehaviour {
+
+	public PrologueGameplay gameplay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			gameplay.Progress();
+			this.gameObject.SetActive(false);
+		}
+	}
+}
