404efcc8a92369c1574f30651b8df9e5855eb64f
jsohn96 <jsohn96@hotmail.com>
2015-12-02 21:02:31 -0800

    RockerFall Sprite Added

--- The Prince and the Skeleton/Assets/Scripts/Rockerfall.cs
+++ The Prince and the Skeleton/Assets/Scripts/Rockerfall.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Rockerfall : MonoBehaviour {
-
+	Animator animator;
 	float loopTime = 4f;
 	float fallTime = 1f;
 
@@ -10,16 +10,19 @@ public class Rockerfall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		animator = this.gameObject.transform.GetChild(1).GetComponent<Animator>();
 		StopFall ();
 	}
 
 	void StartFall() {
+		//animator.SetBool("fall", true);
 		visuals.SetActive(true);
 		collisionArea.SetActive(true);
 		Invoke ("StopFall", fallTime);
 	}
 
 	void StopFall() {
+		animator.SetBool("fall", false);
 		visuals.SetActive(false);
 		collisionArea.SetActive(false);
 		Invoke ("StartFall", loopTime - fallTime);
